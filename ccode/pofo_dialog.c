5b057302de521cd81708631fc0698253dfdad03e
Petr Kracik <petrkr@petrkr.net>
2026-09-25 23:32:37 +0200

    msg dialog: separated text and title

--- ccode/pofo_dialog.c
+++ ccode/pofo_dialog.c
@@ -4,15 +4,34 @@
 static unsigned int pofo_dialog_top_left;
 static unsigned int pofo_dialog_action;
 static char *pofo_dialog_text;
+static char pofo_dialog_rom_text[256];
 
-static void pofo_dialog_call(action, top_left, text)
+static void pofo_dialog_build(title, text)
+char *title;
+char *text;
+{
+    unsigned int title_length;
+    unsigned int text_length;
+
+    title_length = strlen(title);
+    text_length = strlen(text);
+    memcpy(pofo_dialog_rom_text, title, title_length);
+    pofo_dialog_rom_text[title_length] = 0;
+    memcpy(pofo_dialog_rom_text + title_length + 1, text, text_length);
+    pofo_dialog_rom_text[title_length + text_length + 1] = 0;
+    pofo_dialog_rom_text[title_length + text_length + 2] = 0;
+}
+
+static void pofo_dialog_call(action, top_left, title, text)
 unsigned int action;
 unsigned int top_left;
+char *title;
 char *text;
 {
     pofo_dialog_action = action;
     pofo_dialog_top_left = top_left;
-    pofo_dialog_text = text;
+    pofo_dialog_build(title, text);
+    pofo_dialog_text = pofo_dialog_rom_text;
 
 #asm
     push ax
@@ -31,33 +50,35 @@ char *text;
 #endasm
 }
 
-void pofo_message_dialog(top_left, text)
+void pofo_message_dialog(top_left, title, text)
 unsigned int top_left;
+char *title;
 char *text;
 {
-    pofo_dialog_call(0x1200, top_left, text);
+    pofo_dialog_call(0x1200, top_left, title, text);
 }
 
-void pofo_error_dialog(top_left, text)
+void pofo_error_dialog(top_left, title, text)
 unsigned int top_left;
+char *title;
 char *text;
 {
-    pofo_dialog_call(0x1400, top_left, text);
+    pofo_dialog_call(0x1400, top_left, title, text);
 }
 
-/* text is title\0body\0\0 (same format as pofo_message_dialog/
-   pofo_error_dialog). Returns the bottom_right corner of the box the
+/* Returns the bottom_right corner of the box the
    ROM will draw at top_left: two text lines (title, body) plus a
    border row above and below, width is the longer line plus one
    border column on each side. */
-unsigned int pofo_dialog_extent(top_left, text)
+unsigned int pofo_dialog_extent(top_left, title, text)
 unsigned int top_left;
+char *title;
 char *text;
 {
     unsigned char title_len, body_len, width;
 
-    title_len = (unsigned char)strlen(text);
-    body_len = (unsigned char)strlen(text + title_len + 1);
+    title_len = (unsigned char)strlen(title);
+    body_len = (unsigned char)strlen(text);
     width = (title_len > body_len ? title_len : body_len) + 4;
 
     return POFO_COORD(POFO_COORD_ROW(top_left) + 3,
